a27b44a1df1bef9d3f8c59e70f5418aee970f614
scetre <scetre@protonmail.com>
2022-06-23 15:56:54 +0100

    creation

---
+++ include/daoBase.h
@@ -0,0 +1,54 @@
+/**
+ * @file    daoBase.c
+ * @brief   Durham AO RTC library
+ * 
+ * Durham AO RTC library description file. 
+ *  
+ * @author  S. Cetre
+ * @date    23/06/2022
+ *
+ * 
+ */
+
+#ifndef _DAOBASE_H
+#define _DAOBASE_H
+
+#define DAO_SUCCESS 0
+#define DAO_ERROR   1
+
+#define DAO_WARNING 0
+#define DAO_INFO 1
+#define DAO_DEBUG 2
+#define DAO_TRACE 3
+
+int krtcLogLevel = 1;
+#define krtcError(fmt, ...) fprintf(stderr, "%s [error] %s:%d: " fmt, krtcGetTimeStamp(), __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define	krtcPrint(fmt, ...) \
+            do { fprintf(stdout, "%s:%d: " fmt, __FUNCTION__, __LINE__, ##__VA_ARGS__); } while(0)
+#define	krtcWarning(fmt, ...) \
+            do { if (krtcLogLevel>=DAO_WARNING) fprintf(stdout, "%s [warning] %s:%d: " fmt, krtcGetTimeStamp(), __FUNCTION__, __LINE__, ##__VA_ARGS__); } while(0)
+#define	krtcInfo(fmt, ...) \
+            do { if (krtcLogLevel>=DAO_INFO) fprintf(stdout, "%s [info] %s:%d: " fmt, krtcGetTimeStamp(), __FUNCTION__, __LINE__, ##__VA_ARGS__); } while(0)
+#define	krtcDebug(fmt, ...) \
+            do { if (krtcLogLevel>=DAO_DEBUG) fprintf(stdout, "%s [debug] %s:%d: " fmt, krtcGetTimeStamp(), __FUNCTION__, __LINE__, ##__VA_ARGS__); } while(0)
+#define krtcTrace(fmt, ...) \
+            do { if (krtcLogLevel>=DAO_TRACE) fprintf(stdout, "%s [trace] %s:%d " fmt, daoGetTimeStamp(), __FUNCTION__, __LINE__, ##__VA_ARGS__); } while (0)
+
+
+#include "daoImageStruct.h" // data structure
+
+char * daoGetTimeStamp();
+
+int_fast8_t daoInit1D(const char *name, char *prefix, int nbVal, IMAGE **image);
+int_fast8_t daoShm2Img(const char *name, char *prefix, IMAGE *image);
+int_fast8_t daoImage2Shm(float *procim, int nbVal, IMAGE *image); 
+int_fast8_t daoImagePart2Shm(float *procim, int nbVal, IMAGE *image, int position); 
+int_fast8_t daoImagePart2ShmFinalize(IMAGE *image); 
+int_fast8_t daoImage2ShmUI16(unsigned short *procim, int nbVal, IMAGE *image); 
+int_fast8_t daoImage2ShmSI16(short *procim, int nbVal, IMAGE *image); 
+int_fast8_t daoImage2ShmUI32(unsigned int *procim, int nbVal, IMAGE *image); 
+int_fast8_t daoImage2ShmSI32(int *procim, int nbVal, IMAGE *image); 
+int_fast8_t krtcImageCreateSem(IMAGE *image, long NBsem);
+int_fast8_t krtcImageCreate(IMAGE *image, const char *name, long naxis, uint32_t *size, uint8_t atype, int shared, int NBkw);
+
+#endif
---
+++ wscript
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# encoding: utf-8
+# Sylvain Cetre
+import os
+# the following two variables are used by the target "waf dist"
+VERSION='0.0.1'
+APPNAME='daoBase'
+
+# if you want to cross-compile, use a different command line:
+# CC=mingw-gcc AR=mingw-ar waf configure build
+
+top = '.'
+
+from waflib import Configure, Logs, Utils, Context
+#Configure.autoconfig = True # True/False/'clobber'
+
+def options(opt):
+	opt.load('compiler_c gnu_dirs')
+
+def configure(conf):
+	conf.load('compiler_c gnu_dirs')
+	conf.write_config_header('config.h')
+	print('→ prefix is ' + conf.options.prefix)
+
+def build(bld):
+	bld.env.DEFINES=['WAF=1']
+	bld.recurse('src')#apps')
+	#bld.install_files('/tmp/foo', 'wscript')
+	bld.install_files(bld.env.PREFIX+'/include', 'include/daoBase.h', relative_trick=False)
+	bld.install_files(bld.env.PREFIX+'/include', 'include/daoImageStruct.h', relative_trick=False)
