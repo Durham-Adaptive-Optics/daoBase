f3ef2642e8667f1d987af7ee8b2c80e57b191008
dbarr <david.barr@durham.ac.uk>
2023-01-25 17:06:42 +0000

    Modifed daoLog to use the default python logger and extend its functionality

--- src/python/daoLog.py
+++ src/python/daoLog.py
@@ -1,123 +1,91 @@
 import socket
 from datetime import datetime
+import logging
 
 # this is the network logger. Maybe we can encapsulate this better. 
 import zmq
 import daoLogging_pb2
+import time
 
 
-class daoLog:
-    def __init__(self, name, dst="SCREEN", level="TRACE", ip=None, port=None, filename=None):
-        self.name = name
-        self.hostname = socket.gethostname()
-        self.dst = dst
-        if(dst == "SCREEN"):
+class daoLog(logging.Logger):
+    TRACE = 5
+    def __init__(self, name, level=TRACE, ip=None, port=None, machine=None, logfile=None):
+        super().__init__(name)
+        logging.addLevelName(self.TRACE, "TRACE")
+        self.setLevel(level)
+        self.propagate = True
+        handler = logging.StreamHandler()
+        handler.setLevel(level)
+        
+        self.ip = ip
+        self.port = port
+        self.machine = machine
+        if self.machine is None:
+            self.machine = socket.gethostname()
+
+        formatter = logging.Formatter('[%(asctime)s] %(machine)s - %(name)s [%(levelname)s] : %(message)s')
+        handler.setFormatter(formatter)
+        handler.addFilter(ContextFilter(self.machine))
+        self.addHandler(handler)
+
+        if(self.port is None or self.ip is None or self.machine is None):
             pass
-        if(dst == "FILE"):
-            # open file
-            self.file = open(self.filename, "a")
-
-        if(dst == "NETWORK"):
-            self.ip = ip
-            self.port = port
-            connect_string = f'tcp://{ip}:{port}'
-            print(f"Connecting to : {connect_string}")
-            self.network_context = zmq.Context()
-            self.network_socket = self.network_context.socket(zmq.PUB)
-            self.network_socket.connect(connect_string)
-            # create a empty log_message that is configured at will
-            self.network_log_message = daoLogging_pb2.Logs()
-            self.network_log_mess = self.network_log_message.logs.add()
-            self.network_log_mess.component_name = self.name
-            self.network_log_mess.machine = self.hostname
-        self.level = self.LevelStringToValue(level)
-
-
-        # configure depending dst
-    def log(self, string, level):
-        timestamp =datetime.now()
-        ts= timestamp.strftime("%d-%b-%Y (%H:%M:%S.%f)")
-        tmp = f"[{self.hostname}] {ts} [{self.name}] [{level}] - {string}"
-        if(self.dst == "SCREEN"):
-            print(tmp)
-        elif(self.dst == "FILE"):
-            self.write(tmp)
-        elif(self.dst == "NETWORK"):
-            self.writeToNetwork(string, level, ts)
-
-
-    def Trace(self, string):
-        if(self.level <= 0):
-            self.log(string, "TRACE")
-    
-    def Debug(self, string):
-        if(self.level <= 1):
-            self.log(string, "DEBUG")
-
-    def Info(self, string):
-        if(self.level <= 2):
-            self.log(string,"INFO")
-
-    def Warning(self, string):
-        if(self.level <= 3):
-            self.log(string, "WARNING")
-
-    def Error(self, string):
-        if(self.level <= 4):
-            self.log(string, "ERROR")
-
-    def Fatal(self, string):
-        if(self.level <= 5):
-            self.log(string, "FATAL")
-
-    def setLogLevel(self, level):
-        self.level = self.LevelStringToValue(level)
-        self.Debug(f"Log level set to {self.LevelValueToString(self.level)}")
-
-    def LevelStringToValue(self, levelString):
-        if(levelString == "TRACE"):
-            return 0
-        elif(levelString == "DEBUG"):
-            return 1
-        elif(levelString == "INFO"):
-            return 2
-        elif(levelString == "WARNING"):
-            return 3
-        elif(levelString == "ERROR"):
-            return 4
-        elif(levelString == "FATAL"):
-            return 5
-    
-
-    def LevelValueToString(self, value):
-        if(value == 0):
-            return "TRACE"
-        elif(value == 1):
-            return "DEBUG"
-        elif(value == 2):
-            return "INFO"
-        elif(value == 3):
-            return "WARNING"
-        elif(value == 4):
-            return "ERROR"
-        elif(value == 5):
-            return "FATAL"
         else:
-            return "UNKNOWN LEVEL"
+            print("d")
+            zmqHandle = daoLogZmqHandler(ip=self.ip, port=self.port, machine=self.machine)
+            self.addHandler(zmqHandle)
+            
+        if logfile is not None:
+            file_handler = logging.FileHandler(file)
+            file_handler.addFilter(ContextFilter(self.machine))
+            file_handler.setFormatter(formatter)
+            self.addHandler(file_handler)
+        
+        logging.Logger.trace = self.trace
 
-    def writeToNetwork(self, string, level, ts):
-        self.network_log_mess.level = level
-        self.network_log_mess.log_message = string
-        self.network_log_mess.time_stamp = ts
-        sendString = self.network_log_message.SerializeToString()
-        self.network_socket.send(sendString)
+    def trace(self, msg, *args, **kwargs):
+        if self.isEnabledFor(self.TRACE):
+            self._log(self.TRACE, msg, args, **kwargs)
+
+class daoLogZmqHandler(logging.Handler):
+    def __init__(self, ip, port, machine):
+        super().__init__()
+        self.ip = ip
+        self.port = port
+        self.machine = machine
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.PUB)
+        self.socket.connect(f"tcp://{self.ip}:{self.port}")
+
+    def emit(self, record):
+        log_message = daoLogging_pb2.Logs()
+        log_mess = log_message.logs.add()
+        log_mess.level = record.levelname
+        log_mess.log_message = record.msg
+        log_mess.time_stamp = str(record.created)
+        log_mess.component_name = record.name
+        log_mess.machine = record.machine
+        self.socket.send(log_message.SerializeToString())
+
+
+class ContextFilter(logging.Filter):
+    def __init__(self, machine:str):
+        super().__init__()
+        self.machine = machine
+    def filter(self, record):
+        record.machine = self.machine
+        return True
 
 if __name__=="__main__":
-    logger = daoLog("test")
-    logger.setLogLevel("INFO")
-    logger.Trace("T")
-    logger.Debug("T")
-    logger.Info("T")
-    logger.Warning("T")
-    logger.Error("T")
-    logger.Fatal("T")
+    file = "log.txt"
+    ip = '127.0.0.1'
+    port = 5555
+    log = daoLog(__name__, logfile=file ,port=port, ip=ip)
+
+    log.trace("This is a trace message")
+    log.debug("This is a debug message")
+    log.info("This is a info message")
+    log.warning("This is a warning message")
+    log.error("This is a error message ")
+    log.critical("This is a critical message")
