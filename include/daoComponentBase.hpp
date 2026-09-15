77a648d169081a1358672951d3805443d9f12f08
David Barr <31646965+davetbarr@users.noreply.github.com>
2026-09-15 14:16:30 +0100

    Suppress warnings in Log function
    Commented out the payload in Log function to suppress warnings.

--- include/daoComponentBase.hpp
+++ include/daoComponentBase.hpp
@@ -91,7 +91,7 @@ namespace Dao
             void Idle(){};
             void OnFailure(){};
             void Recover(){};
-            void Log(std::string /*message*/){};
+            void Log(std::string /*message*/){}; // commented out payload to supress warnings should not effect anything downstream but beaware.
 
             virtual void PROCESS_OTHER(std::string Payload)
             {
